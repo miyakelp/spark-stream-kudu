b88e00eecd5cc5f216788af5ee72416d4fd6edc1
miyakelp <lisknkknk.al.g2k13+miyakelp@gmail.com>
2020-10-04 20:24:28 +0900

    書き込み

--- build.sbt
+++ build.sbt
@@ -1,13 +1,15 @@
 name := "import-user-log-sample"
 version := "0.0.1-SNAPSHOT"
-scalaVersion := "2.12.10"
+scalaVersion := "2.11.12"
 organization := "sparksample"
 scalacOptions ++= Seq("-Xlint", "-deprecation", "-unchecked", "-feature")
 updateOptions := updateOptions.value.withCachedResolution(true)
 
 libraryDependencies ++= Seq(
-  "org.apache.spark" %% "spark-core" % "3.0.1",
-  "org.apache.spark" %% "spark-streaming" % "3.0.1",
+  "org.apache.spark" %% "spark-core" % "2.4.7",
+  "org.apache.spark" %% "spark-sql" % "2.4.7",
+  "org.apache.spark" %% "spark-streaming" % "2.4.7",
+  "org.apache.kudu"  %% "kudu-spark2" % "1.13.0",
 )
 
 assemblyMergeStrategy in assembly := {
@@ -16,9 +18,11 @@ assemblyMergeStrategy in assembly := {
   case PathList(ps @ _*) if ps.last endsWith ".xml" => MergeStrategy.first
   case PathList(ps @ _*) if ps.last endsWith ".types" => MergeStrategy.first
   case PathList(ps @ _*) if ps.last endsWith ".class" => MergeStrategy.first
+  case PathList(ps @ _*) if ps.last endsWith "mailcap" => MergeStrategy.first
+  case PathList(ps @ _*) if ps.last endsWith ".jnilib" => MergeStrategy.first
+  case PathList(ps @ _*) if ps.last endsWith ".so" => MergeStrategy.first
   case "application.conf"                            => MergeStrategy.concat
   case "unwanted.txt"                                => MergeStrategy.discard
-  case PathList(ps @ _*) if ps.last endsWith "mailcap" => MergeStrategy.first
   case x =>
     val oldStrategy = (assemblyMergeStrategy in assembly).value
     oldStrategy(x)
--- src/main/scala/sparksample/ImportUserLog.scala
+++ src/main/scala/sparksample/ImportUserLog.scala
@@ -1,19 +1,49 @@
 package sparksample
 
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql._
+import org.apache.spark.sql.types._
+import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.StreamingContext._
+import org.apache.kudu.spark.kudu._
+import org.apache.kudu.client._
+
+case class Log(id:String, unixTime:Int, userId:Int, score:Int)
 
 
 object ImportUserLog {
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("ImportUserLog").setMaster("spark://kudu01:7077")
-    val sc = new SparkContext(conf)
+    val spark = SparkSession
+      .builder()
+      .appName("ImportUserLog")
+      .getOrCreate()
+    val sc = spark.sparkContext
     val ssc = new StreamingContext(sc, Seconds(1))
+    import spark.implicits._
+
+    val kc = new KuduContext("kudu01:7051", sc)
+
+    val schema = StructType(List(
+      StructField("id", StringType, nullable = false),
+      StructField("unix_time", StringType, nullable = true),
+      StructField("user_id", StringType, nullable = true),
+      StructField("score", StringType, nullable = true)
+    ))
 
     val lines = ssc.socketTextStream("kudu01", 12345)
-    lines.map(_.split(",")).map(x => (x(1), x)).reduceByKey((x, y) => y).map(_._2).foreachRDD(rdd => rdd.foreach(x => println(x(1))))
+    lines
+      .map(_.split(","))
+      .map(x => (x(1), x))
+      .reduceByKey((x, y) => y)
+      .map(_._2)
+      .foreachRDD(rdd => rdd.foreach( x => {
+          val row = sc.parallelize(Seq(Row(x(1), x(0).toInt, x(2).toInt, x(3).toInt)))
+          val df = spark.createDataFrame(row, schema)
+          kc.insertRows(df, "user_logs")
+        })
+      )
 
     ssc.start()
 
